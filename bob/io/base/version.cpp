74f797e540f4a48b5f434bbf4cef31825e96cf6d
Manuel Gunther <siebenkopf@googlemail.com>
2015-12-03 12:30:31 -0700

    Corrected error cases in module creation

--- bob/io/base/version.cpp
+++ bob/io/base/version.cpp
@@ -5,7 +5,6 @@
  * @brief Binds configuration information available from bob
  */
 
-
 #define BOB_IMPORT_VERSION
 #include <bob.blitz/config.h>
 #include <bob.blitz/cleanup.h>
@@ -53,11 +52,13 @@ static PyObject* create_module (void) {
 
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* m = PyModule_Create(&module_definition);
+  auto m_ = make_xsafe(m);
+  const char* ret = "O";
 # else
   PyObject* m = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!m) return 0;
-  auto m_ = make_safe(m); ///< protects against early returns
 
   /* register version numbers and constants */
   if (PyModule_AddIntConstant(m, "api", BOB_IO_BASE_API_VERSION) < 0)
@@ -69,8 +70,7 @@ static PyObject* create_module (void) {
   // call bob_io_base_version once to avoid compiler warning
   auto _ = make_safe(bob_io_base_version());
 
-  return Py_BuildValue("O", m);
-
+  return Py_BuildValue(ret, m);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
